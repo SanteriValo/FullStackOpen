bc42db736829f8cc237ab5025a42dd62e3b96b08
SanteriValo <valosanteri@gmail.com>
2025-01-15 19:16:15 +0200

    1.4: Course Information step 4 is completed

--- part 1/courseinfo/src/App.jsx
+++ part 1/courseinfo/src/App.jsx
@@ -1,61 +1,47 @@
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = {
+    const parts = [
+    {
         name: 'Fundamentals of React',
         exercises: 10
-    }
-    const part2 = {
+    },
+    {
         name: 'Using props to pass data',
         exercises: 7
-    }
-    const part3 = {
+    },
+    {
         name: 'State of a component',
         exercises: 14
-    }
+    },
+    ]
     return (
         <div>
             <Header course={course}/>
-            <Content
-                part1 = {part1}
-                part2 = {part2}
-                part3 = {part3}
-            />
-            <Total
-                part1 = {part1}
-                part2 = {part2}
-                part3 = {part3}
-            />
+            <Content parts={parts}/>
+            <Total parts={parts}/>
         </div>
     )
 }
 
 const Header = (props) => {
-    console.log(props)
     return <h1>{props.course}</h1>
 }
 
-const Content = ({part1, part2, part3}) => {
+const Content = ({parts}) => {
     return (
         <div>
-            <Part part={part1} />
-            <Part part={part2} />
-            <Part part={part3} />
+            <p>{parts[0].name} {parts[0].exercises}</p>
+            <p>{parts[1].name} {parts[1].exercises}</p>
+            <p>{parts[2].name} {parts[2].exercises}</p>
         </div>
     );
 };
 
-const Part = ({part}) => {
+const Total = ({parts}) => {
+    const TotalExercises = (parts[0].exercises + parts[1].exercises + parts[2].exercises);
     return (
         <p>
-            {part.name} {part.exercises}
-        </p>
-    );
-};
-
-const Total = ({part1, part2, part3}) => {
-    return (
-        <p>
-            Number of exercises: {part1.exercises + part2.exercises + part3.exercises}
+            Number of exercises: {TotalExercises}
         </p>
     );
 };
