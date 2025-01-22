99df68c44da70093f8d956027d2e0cd4bd2e749d
SanteriValo <valosanteri@gmail.com>
2025-01-22 18:29:44 +0200

    1.11*: unicafe step 6

--- part 1/unicafe/src/App.jsx
+++ part 1/unicafe/src/App.jsx
@@ -68,21 +68,26 @@ const Statistics = (props) => {
     return (
         <div>
             <h1>statistics</h1>
-            <StatisticLine text="good" value={props.good} />
-            <StatisticLine text="neutral" value={props.neutral} />
-            <StatisticLine text="bad" value={props.bad} />
-            <StatisticLine text="all" value={props.total} />
-            <StatisticLine text="average" value={props.average} />
-            <StatisticLine text="positive" value={props.positive} />
+            <table>
+                <tbody>
+                <StatisticLine text="good" value={props.good}/>
+                <StatisticLine text="neutral" value={props.neutral}/>
+                <StatisticLine text="bad" value={props.bad}/>
+                <StatisticLine text="all" value={props.total}/>
+                <StatisticLine text="average" value={props.average}/>
+                <StatisticLine text="positive" value={props.positive}/>
+                </tbody>
+            </table>
         </div>
     )
 }
 
-const StatisticLine = ({ text, value }) => {
+const StatisticLine = ({text, value}) => {
     return (
-        <p>
-            {text}: {value}
-        </p>
+        <tr>
+            <td>{text}:</td>
+            <td>{value}</td>
+        </tr>
     )
 }
 
