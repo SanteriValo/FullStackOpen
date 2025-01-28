0c96dd8ace09763448d053f9346cd14de31b61bc
SanteriValo <valosanteri@gmail.com>
2025-01-28 18:11:35 +0200

    2.2: Course information step 7

--- part2/courseInformation/src/components/Content.jsx
+++ part2/courseInformation/src/components/Content.jsx
@@ -1,11 +1,16 @@
 import Part from "./Part.jsx";
 
-const Content = (props) => (
+const Content = (props) => {
+    const total = props.parts.reduce((sum, x) => sum + x.exercises, 0)
+    return (
     <div>
-        <Part part={props.parts[0]} />
-        <Part part={props.parts[1]} />
-        <Part part={props.parts[2]} />
+        <Part part={props.parts[0]}/>
+        <Part part={props.parts[1]}/>
+        <Part part={props.parts[2]}/>
+        <Part part={props.parts[3]}/>
+        <p><b>total of {total} exercises</b></p>
     </div>
-)
+    )
+}
 
 export default Content
