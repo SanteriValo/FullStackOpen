6ec7830e0f1d8e12d0e042637360b19303c76395
SanteriValo <valosanteri@gmail.com>
2025-03-25 19:58:35 +0200

    3.13: Phonebook database, step 1

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express();
+const mongoose = require('mongoose');
 
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
 app.use(cors());
 app.use(express.json());
 app.use(express.static('dist'))
@@ -11,35 +15,43 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
     skip: (req) => req.method === "GET"
 }));
 
-let phones = [
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    },
-]
+if (!password) {
+    console.log("No password");
+    process.exit(1);
+}
+
+const encodedPassword = encodeURIComponent(password);
+const url = `mongodb+srv://fullstack:${encodedPassword}@cluster0.vg0i1.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+    .then(() => console.log("Connected"))
+    .catch(err => console.log('Connection error:', err));
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+});
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject.id
+        delete returnedObject._id
+        delete returnedObject.__v;
+    }
+});
+
+const Person = mongoose.model('Person', personSchema);
 
 app.get('/', (request, response) => {
     response.send('<h1>Phonebook</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(phones)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/info', (request, response) => {
