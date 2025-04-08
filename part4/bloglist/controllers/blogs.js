bddf789cdfe9b8816356dc6842adb23388db0ec6
SanteriValo <valosanteri@gmail.com>
2025-04-08 16:11:11 +0300

    4.2 Blog List, step 2

---
+++ part4/bloglist/controllers/blogs.js
@@ -0,0 +1,19 @@
+const Blog = require('../models/blog')
+
+const getAllBlogs = (req, res) => {
+    Blog.find({}).then((blogs) => {
+        res.json(blogs)
+    })
+}
+
+const createBlog = (req, res) => {
+    const blog = new Blog(req.body)
+    blog.save().then((result) => {
+        res.status(201).json(result)
+    })
+}
+
+module.exports = {
+    getAllBlogs,
+    createBlog,
+}
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,17 +1,9 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const blogController = require('./controllers/blogs')
 
 const app = express()
-
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = process.env.MONGODB_URI
 if (!mongoUrl) {
@@ -29,19 +21,8 @@ mongoose.connect(mongoUrl)
 
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-    Blog.find({}).then((blogs) => {
-        response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog.save().then((result) => {
-        response.status(201).json(result)
-    })
-})
+app.get('/api/blogs', blogController.getAllBlogs)
+app.post('/api/blogs', blogController.createBlog)
 
 const PORT = 3003
 app.listen(PORT, () => {
