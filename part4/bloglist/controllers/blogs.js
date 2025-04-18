34b27150d9c54abde2cdd30197df833bc9c1e6a1
SanteriValo <valosanteri@gmail.com>
2025-04-18 21:46:15 +0300

    4.19: Blog List Expansion, step 7

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
@@ -10,9 +11,23 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
     const body = request.body;
 
-    const user = await User.findOne({});
+    const authorization = request.get('authorization');
+    if (!authorization || !authorization.toLowerCase().startsWith('bearer ')) {
+        return response.status(401).json({ error: 'token missing' });
+    }
+    const token = authorization.substring(7);
+
+    let decodedToken;
+    try {
+        decodedToken = jwt.verify(token, process.env.SECRET);
+        console.log('Decoded Token:', decodedToken);
+    } catch (error) {
+        return response.status(401).json({ error: 'token invalid' });
+    }
+
+    const user = await User.findById(decodedToken.id);
     if (!user) {
-        return response.status(400).json({ error: 'no users found in database' });
+        return response.status(401).json({ error: 'user not found' });
     }
 
     const blog = new Blog({
