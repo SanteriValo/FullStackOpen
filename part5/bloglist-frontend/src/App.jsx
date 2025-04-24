e590f0ea547cb87b6d270bb6589f614ccb7d0900
SanteriValo <valosanteri@gmail.com>
2025-04-24 22:02:36 +0300

    5.2: Blog List Frontend, step 2

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,45 +1,58 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
 import loginService from './services/login'
-import LoginForm from "./components/LoginForm.jsx";
 
 const App = () => {
   const [user, setUser] = useState(null)
   const [blogs, setBlogs] = useState([])
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+      blogService.getAll().then(blogs => setBlogs(blogs))
+    }
   }, [])
 
-  const handleLogin = async (credentials) => {
+  const handleLogin = async (username, password) => {
     try {
-      const user = await loginService.login(credentials)
+      const user = await loginService.login({ username, password })
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+      blogService.setToken(user.token)
       setUser(user)
-    } catch  (exception) {
-      alert('wrong credentials')
+      const blogs = await blogService.getAll()
+      setBlogs(blogs)
+    } catch (exception) {
+      alert('Wrong credentials')
     }
   }
 
-  if (user === null) {
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
+  }
+
+  if (!user) {
     return (
         <div>
-          <h2>Login to the app</h2>
+          <h2>Log in to application</h2>
           <LoginForm handleLogin={handleLogin} />
         </div>
     )
   }
 
   return (
-    <div>
-      <h2>blogs</h2>
-      <p>{user.name} logged in</p>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
+      <div>
+        <h2>blogs</h2>
+        <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
+        {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
   )
 }
 
--- part5/bloglist-frontend/src/components/LoginForm.jsx
+++ part5/bloglist-frontend/src/components/LoginForm.jsx
@@ -1,25 +1,37 @@
-import {useState} from "react";
+import { useState } from 'react'
 
 const LoginForm = ({ handleLogin }) => {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
 
-    const submit = (event) => {
+    const onSubmit = (event) => {
         event.preventDefault()
-        handleLogin({username, password})
+        handleLogin(username, password)
         setUsername('')
         setPassword('')
     }
 
     return (
-        <form onSubmit={submit}>
+        <form onSubmit={onSubmit}>
             <div>
-                username <input value={username} onChange={({target}) => setUsername(target.value)} />
+                username
+                <input
+                    type="text"
+                    value={username}
+                    name="Username"
+                    onChange={({ target }) => setUsername(target.value)}
+                />
             </div>
             <div>
-                password <input type="password" value={password} onChange={({target}) => setPassword(target.value)} />
+                password
+                <input
+                    type="password"
+                    value={password}
+                    name="Password"
+                    onChange={({ target }) => setPassword(target.value)}
+                />
             </div>
-            <button type="submit">Login...</button>
+            <button type="submit">login</button>
         </form>
     )
 }
